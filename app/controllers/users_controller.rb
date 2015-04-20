7c43fa2d60879dfb78e9e28be83f3c63611ea8f4
Hippie <grwarren@thoughtworks.com>
2015-04-19 23:16:06 -0300

    Added user creating and viewing

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path, notice: 'New User Created'
+    else
+      render new_user_path
+    end
+  end
+
+  def new
+    @user =  User.new
+  end
+
+  def edit
+  end
+
+
+  def update
+  end
+
+  def index
+    @users = User.all
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_id, :name)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   resources :orders
 
-
+   resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
