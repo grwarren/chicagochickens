3015cd42f1f1947a584ef614664db417b0a86603
Mukiza Andrew <andrewmukiza@gmail.com>
2015-04-21 22:17:32 -0300

    adds more controller tests asserting saving of a new order and the ordered product

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -7,7 +7,7 @@ describe OrdersController, :type => :controller do
       @products = create_list :product, 2
     end
 
-    it 'should show new order page' do
+    it 'shows new order page with new order and products' do
       get :new
 
       expect(response).to have_http_status(:success)
@@ -22,11 +22,21 @@ describe OrdersController, :type => :controller do
     let(:user) { create :user }
     let(:product) { create :product }
 
-    it 'should show new order page' do
+    it 'saves new order' do
+      expect {  post :create,  order: { quantity: 10, product: product.id } , user_id: user.user_id }.to change(Order, :count).by(1)
+    end
+
+    it 'saves order with ordered product' do
+      post :create,  order: { quantity: 10, product: product.id } , user_id: user.user_id
+
+      expect(assigns(:order).reload.product).to eql product
+    end
+
+    it 'redirects to new order page after save' do
       post :create,  order: { quantity: 10, product: product.id } , user_id: user.user_id
 
       expect(flash[:notice]).to eql("Your order has been recieved")
-      expect(response).to redirect_to user_orders_path
+      expect(response).to redirect_to user_orders_path(user_id: user.user_id)
     end
   end
 end
