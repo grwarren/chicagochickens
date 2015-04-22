409284a7a4d65af593dde02c5f8537028d41dca5
Mukiza Andrew <andrewmukiza@gmail.com>
2015-04-21 22:59:56 -0300

    add validation check on the create order action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,10 @@
 class OrdersController < ApplicationController
   before_action :set_current_user
+  before_action :set_products, only: [:new, :create]
 
   def new
     @order = Order.new
     @next_delivery_date = 1.week.from_now.strftime("%m-%d-%Y")
-    @products = Product.all
   end
 
    def index
@@ -12,18 +12,28 @@ class OrdersController < ApplicationController
    end
 
   def create
-    product = Product.find_by(id: params[:order][:product])
-    @order = Order.new(order_params.merge(user: @current_user, product: product))
+    @order = Order.new(order_params.merge(user: @current_user))
+    product_id = params[:order][:product]
+    unless product_id.empty?
+      product = Product.find_by(id: product_id)
+      @order.product = product
 
-    if @order.save
-      redirect_to user_orders_path(user_id: @current_user.user_id) , notice: 'Your order has been recieved'
+      if @order.save
+        redirect_to user_orders_path(user_id: @current_user.user_id) , notice: 'Your order has been recieved'
+      else
+        render :new
+      end
     else
-      render redirect_to user_orders_path(user_id: @current_user.user_id)
+      render :new
     end
   end
 
 private
   def order_params
     params.require(:order).permit(:quantity, :delivery_date)
+  end
+
+  def set_products
+    @products = Product.all
   end
 end
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -21,22 +21,35 @@ describe OrdersController, :type => :controller do
   describe 'POST /create' do
     let(:user) { create :user }
     let(:product) { create :product }
+    describe 'valid params' do
+      it 'saves new order' do
+        expect {  post :create,  order: { quantity: 10, product: product.id } , user_id: user.user_id }.to change(Order, :count).by(1)
+      end
 
-    it 'saves new order' do
-      expect {  post :create,  order: { quantity: 10, product: product.id } , user_id: user.user_id }.to change(Order, :count).by(1)
+      it 'saves order with ordered product' do
+        post :create,  order: { quantity: 10, product: product.id } , user_id: user.user_id
+
+        expect(assigns(:order).reload.product).to eql product
+      end
+
+      it 'redirects to new order page after save' do
+        post :create,  order: { quantity: 10, product: product.id } , user_id: user.user_id
+
+        expect(flash[:notice]).to eql("Your order has been recieved")
+        expect(response).to redirect_to user_orders_path(user_id: user.user_id)
+      end
     end
 
-    it 'saves order with ordered product' do
-      post :create,  order: { quantity: 10, product: product.id } , user_id: user.user_id
+    describe 'invalid params' do
+      it 'renders new with errors' do
+        post :create,  order: { quantity: 10, product: '' } , user_id: user.user_id
+        expected_errors = ["Product can't be blank"]
 
-      expect(assigns(:order).reload.product).to eql product
-    end
-
-    it 'redirects to new order page after save' do
-      post :create,  order: { quantity: 10, product: product.id } , user_id: user.user_id
-
-      expect(flash[:notice]).to eql("Your order has been recieved")
-      expect(response).to redirect_to user_orders_path(user_id: user.user_id)
+        expect(response).to render_template(:new)
+        expect(assigns :order).to_not be_valid
+        expect(assigns :products).to contain_exactly(product)
+        expect(assigns(:order).errors.full_messages).to match_array(expected_errors)
+      end
     end
   end
 end
