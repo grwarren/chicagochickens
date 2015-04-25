d806166785ddcf48855e2e9b498be5e1b1446e2a
Hippie <grwarren@thoughtworks.com>
2015-04-25 13:23:02 -0300

    Fixed spelling error

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,9 +1,9 @@
 module OrdersHelper
-  def prularize_units(num, product)
-    product.unit.empty? ? "#{num} #{product.name}" : num > 1 ? "#{num} (#{product.unit.pluralize})" : "#{num} (#{product.unit})"
+  def pluralize_units(num, product)
+    product.unit.empty? ? "#{num} #{product.name}" : num > 1 ? "#{num} #{product.unit.pluralize}" : "#{num} #{product.unit}"
   end
 
   def quantity_options_for(product)
-    (0..10).to_a.collect { | n | [" #{prularize_units(n, product) }", n ] }
+    (0..10).to_a.collect { | n | [" #{pluralize_units(n, product) }", n ] }
   end
 end
