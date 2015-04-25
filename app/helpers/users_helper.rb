11e55e87849257a4aee075d519e0c4a36a18ec2b
Mukiza Andrew <andrewmukiza@gmail.com>
2015-04-25 14:59:23 -0300

    adds error safe header on orders

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,8 @@ module UsersHelper
   def build_user_path(user)
     user.nil? ? orders_path : user_orders_path(user)
   end
+
+  def user_orders_headers(user)
+    user.nil? ? "Current Orders" :  "Current Orders for #{user.name}"
+  end
 end
--- spec/helpers/users_helpers_spec.rb
+++ spec/helpers/users_helpers_spec.rb
@@ -13,5 +13,15 @@ describe UsersHelper do
     it 'sets /orders when user :current_user is not set' do
       expect(helper.build_user_path(nil)).to eql(orders_path)
     end
+end
+
+  describe '#user_orders_headers' do
+    it 'shows generic header' do
+      expect(helper.user_orders_headers(nil)).to eql("Current Orders")
+    end
+
+    it 'shows user specific header' do
+      expect(helper.user_orders_headers(current_user)).to eql("Current Orders for #{current_user.name}")
+    end
   end
 end
