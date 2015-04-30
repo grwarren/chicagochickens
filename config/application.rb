d972ff7b9fb06c6f69273f65d0c153a05e780a39
Mukiza Andrew <andrewmukiza@gmail.com>
2015-04-29 23:09:07 -0300

    remove mongo mapper generator config

--- config/application.rb
+++ config/application.rb
@@ -28,9 +28,5 @@ module Chicagochickens
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :deo
-
-    config.generators do |g|
-      g.orm :mongo_mapper
-    end
   end
 end
