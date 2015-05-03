8188428d0d9b8b0b9ad859ec96ca26caa4d8913e
Hippie <grwarren@thoughtworks.com>
2015-05-02 21:17:59 -0300

    added main admin page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'orders#new'
+  root 'main#index'
   get 'orders' => 'orders#index_all'
   post 'orders' => 'orders#create'
   patch '/users/:user_id/orders' => 'orders#update'
