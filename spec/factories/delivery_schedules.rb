cad3b9516be6dce94e37723014fd5974cdd673cd
Greg Warren <grwarren@thoughtworks.com>
2016-05-17 22:10:09 -0400

    fix tests

--- spec/factories/delivery_schedules.rb
+++ spec/factories/delivery_schedules.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :delivery_schedule do
     date 1.week.from_now
+    pickup_location 'TW'
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :user do
     sequence(:user_id) { |n| "10#{n}".to_i }
     name 'Marques Marcello'
+    pickup_location 'TW'
 
     factory :user_with_orders do
       after(:create) do | user|
