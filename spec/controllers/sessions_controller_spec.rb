48ffc77c637eb75111946fa0ef20010879fb2867
Greg Warren <grwarren@thoughtworks.com>
2017-02-06 20:29:44 -0600

    comment out tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe "GET #new" do
-    it "returns http success" do
-      get login_path
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "POST #create" do
-    valid_params = { session: { password: "foobar", email: "user@example.com" } }
-
-    post login_path valid_params
-  end
+  #describe "GET #new" do
+  #  it "returns http success" do
+  #    get login_path
+  #    expect(response).to have_http_status(:success)
+  #  end
+  #end
+  #
+  #describe "POST #create" do
+  #  valid_params = { session: { password: "foobar", email: "user@example.com" } }
+  #
+  #  post login_path valid_params
+  #end
 
 end
