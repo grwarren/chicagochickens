4c218c7cb8d83ee0acb96d55d918cad50386538b
Greg Warren <grwarren@thoughtworks.com>
2017-02-08 19:40:36 -0600

    clean up test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,13 +8,13 @@ describe Product do
   it { is_expected.to validate_presence_of(:name) }
 
   it 'should sort by sort_order' do
-     product1 = build :product
-     product2  = build :product, sort_order: 2
+    product1 = build :product
+    product2 = build :product, sort_order: 2
 
-     products = [product2, product1].sort()
+    products = [product2, product1].sort
 
-     expect(products[0].sort_order).to eq(1)
-     expect(products[1].sort_order).to eq(2)
-   end
+    expect(products[0].sort_order).to eq(1)
+    expect(products[1].sort_order).to eq(2)
+  end
 
 end
